24762ec68c4c5fcf481f16371c51e8161253cb69
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-10-28 20:42:49 -0300

    Aplicando OCP

---
+++ ConsoleApp6/FolhaDePagamento.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class FolhaDePagamento
+    {
+        public static decimal Calcular(Remuneravel remuneravel)
+        {
+            return remuneravel.Remuneracao();
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using OCP;
+using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Remuneravel remuneravel = new Estagio();
+            
+            Console.WriteLine(FolhaDePagamento.Calcular(remuneravel));
+        }
+
+      
+    }
+}
